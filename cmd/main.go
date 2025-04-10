15b71206953ce9466f64d8e5e479790aac0193c1
Ulpio Netto <ulpionetto0@gmail.com>
2025-04-10 01:19:55 -0300

    feat: add routes handler and server startup message in main function

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 
 	"github.com/Ulpio/xuiter.git/internal/config"
+	"github.com/Ulpio/xuiter.git/internal/routes"
 )
 
 func main() {
 	config.ConnectDB()
 	config.AutoMigrate()
 	fmt.Println("Banco Conecta com sucesso")
+	routes.RoutesHandler()
+	fmt.Println("Servidor rodando na porta 8080")
 }
